17f5db74b29a183427e87e2fdf165e44f10402a2
Jacob Berg <jacob.berg.seattle@gmail.com>
2023-06-01 19:44:57 -0700

    Finished HW3

--- policy_gradient.py
+++ policy_gradient.py
@@ -5,13 +5,15 @@ from utils import log_density, rollout
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def train_model(policy, baseline, trajs, policy_optim, baseline_optim, gamma=0.99, baseline_train_batch_size=64, baseline_num_epochs=5):
+
+def train_model(policy, baseline, trajs, policy_optim, baseline_optim, gamma=0.99, baseline_train_batch_size=64,
+                baseline_num_epochs=5):
     # Fill in your policy gradient implementation here
 
-    # TODO: Compute the returns on the current batch of trajectories
-    # Hint: Go through all the trajectories in trajs and compute their return to go: discounted sum of rewards from that timestep to the end. 
-    # Hint: This is easy to do if you go backwards in time and sum up the reward as a running sum. 
-    # Hint: Remember that return to go is return = r[t] + gamma*r[t+1] + gamma^2*r[t+2] + ...
+    # TODO: Compute the returns on the current batch of trajectories Hint: Go through all the trajectories in trajs
+    #  and compute their return to go: discounted sum of rewards from that timestep to the end. Hint: This is easy to
+    #  do if you go backwards in time and sum up the reward as a running sum. Hint: Remember that return to go is
+    #  return = r[t] + gamma*r[t+1] + gamma^2*r[t+2] + ...
     states_all = []
     actions_all = []
     returns_all = []
@@ -21,7 +23,9 @@ def train_model(policy, baseline, trajs, policy_optim, baseline_optim, gamma=0.9
         rewards_singletraj = traj['rewards']
         returns_singletraj = np.zeros_like(rewards_singletraj)
         # TODO start
-            
+        returns_singletraj[len(returns_singletraj) - 1] = rewards_singletraj[len(returns_singletraj) - 1]
+        for i in range(len(returns_singletraj) - 2, -1, -1):
+            returns_singletraj[i] = rewards_singletraj[i] + gamma * returns_singletraj[i + 1]
         # TODO end
         states_all.append(states_singletraj)
         actions_all.append(actions_singletraj)
@@ -29,13 +33,13 @@ def train_model(policy, baseline, trajs, policy_optim, baseline_optim, gamma=0.9
     states = np.concatenate(states_all)
     actions = np.concatenate(actions_all)
     returns = np.concatenate(returns_all)
-    
+
     # TODO: Normalize the returns by subtracting mean and dividing by std
     # Hint: Just do return - return.mean()/ (return.std() + EPS), where EPS is a small constant for numerics
     # TODO start
-    
+    returns = torch.tensor((returns - returns.mean())/(returns.std() + 1e-9)).float().to(device)
     # TODO end
-    
+
     # TODO: Train baseline by regressing onto returns
     # Hint: Regress the baseline from each state onto the above computed return to go. You can use similar code to behavior cloning to do so. 
     # Hint: Iterate for baseline_num_epochs with batch size = baseline_train_batch_size
@@ -48,12 +52,15 @@ def train_model(policy, baseline, trajs, policy_optim, baseline_optim, gamma=0.9
             batch_index = arr[baseline_train_batch_size * i: baseline_train_batch_size * (i + 1)]
             batch_index = torch.LongTensor(batch_index).to(device)
             # TODO start         
-            
+            expert_ret_to_go = returns[batch_index]
+            expert_states = torch.tensor(states).float().to(device)[batch_index]
+            pred_rt_to_go = baseline(expert_states)
+            loss = criterion(pred_rt_to_go, expert_ret_to_go).mean()
             # TODO end
             baseline_optim.zero_grad()
             loss.backward()
             baseline_optim.step()
-            
+
     # TODO: Train policy by optimizing surrogate objective: -log prob * (return - baseline)
     # Hint: Policy gradient is given by: \grad log prob(a|s)* (return - baseline)
     # Hint: Return is computed above, you can computer log_probs using the log_density function imported. 
@@ -64,18 +71,19 @@ def train_model(policy, baseline, trajs, policy_optim, baseline_optim, gamma=0.9
     log_policy = log_density(torch.Tensor(actions).to(device), mu, std, logstd)
     baseline_pred = baseline(torch.from_numpy(states).float().to(device))
     # TODO start
-    
+    loss = (-log_policy * (returns - baseline_pred)).mean()
     # TODO end
-    
+
     policy_optim.zero_grad()
     loss.backward()
     policy_optim.step()
-    
+
     del states, actions, returns, states_all, actions_all, returns_all
 
+
 # Training loop for policy gradient
-def simulate_policy_pg(env, policy, baseline, num_epochs=20000, max_path_length=200, pg_batch_size=100, 
-                        gamma=0.99, baseline_train_batch_size=64, baseline_num_epochs=5, print_freq=10, render=False):
+def simulate_policy_pg(env, policy, baseline, num_epochs=20000, max_path_length=200, pg_batch_size=100,
+                       gamma=0.99, baseline_train_batch_size=64, baseline_num_epochs=5, print_freq=10, render=False):
     policy_optim = optim.Adam(policy.parameters())
     baseline_optim = optim.Adam(baseline.parameters())
 
@@ -86,7 +94,7 @@ def simulate_policy_pg(env, policy, baseline, num_epochs=20000, max_path_length=
         for it in range(pg_batch_size):
             sample_traj = rollout(env=env, agent=policy, episode_length=max_path_length, agent_name='pg', render=render)
             sample_trajs.append(sample_traj)
-        
+
         # Logging returns occasionally
         if iter_num % print_freq == 0:
             rewards_np = np.mean(np.asarray([traj['rewards'].sum() for traj in sample_trajs]))
@@ -94,5 +102,5 @@ def simulate_policy_pg(env, policy, baseline, num_epochs=20000, max_path_length=
             print("Episode: {}, reward: {}, max path length: {}".format(iter_num, rewards_np, path_length))
 
         # Training model
-        train_model(policy, baseline, sample_trajs, policy_optim, baseline_optim, gamma=gamma, 
+        train_model(policy, baseline, sample_trajs, policy_optim, baseline_optim, gamma=gamma,
                     baseline_train_batch_size=baseline_train_batch_size, baseline_num_epochs=baseline_num_epochs)
